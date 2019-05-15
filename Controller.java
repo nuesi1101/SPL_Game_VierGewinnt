07446e5d48f0d53a784342355e79b8ab2bef8fe8
LucaLukas0126 <39698594+LucaLukas0126@users.noreply.github.com>
2019-05-15 15:10:33 +0200

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+
+    @FXML
+    public GridPane root;
+
+    public void handle() {
+
+    }
+}
